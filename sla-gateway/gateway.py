3a2da330f8dc8a930ea07f48b4c4ad1d23fe8b6d
kishore-0501 <kishoreramesh5122@gmail.com>
2026-07-07 11:11:35 +0100

    request rate 35

--- sla-gateway/gateway.py
+++ sla-gateway/gateway.py
@@ -20,7 +20,7 @@ SQS_QUEUE_URL = (
     "https://sqs.eu-west-1.amazonaws.com/238679625965/sla-fallback-queue"
 )
 
-REQUEST_RATE_LIMIT = 40
+REQUEST_RATE_LIMIT = 35
 
 sqs = boto3.client(
     "sqs",
